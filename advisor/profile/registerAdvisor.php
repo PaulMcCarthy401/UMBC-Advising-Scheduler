1c2f391b2cd3fae42c29c4a7e28f0fad8b4e0967
bseipp <brian.seipp@gmail.com>
2016-10-23 18:15:01 -0400

    Add files via upload

--- advisor/profile/registerAdvisor.php
+++ advisor/profile/registerAdvisor.php
@@ -1,6 +1,14 @@
 <?php
 
+require("/~seipp1/class/Advisor.php");
 
+if (!empty($_POST)){
+  //create new advisor object, store post data into object
+  $advisor = new Advisor($_POST['tbFirstName'], $_POST['tbLastName'], $_POST['tbUsername'], $_POST['pwPassword'], $_POST['tbEmail']);
 
+  $advisor.addNewAdvisor();
+
+  require("./template/studentLogin.php");
+}
 
 ?>
