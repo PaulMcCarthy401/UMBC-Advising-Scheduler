70b7f4e3290321beaafd6d5cd5b663370a38216c
Paul McCarthy <armitron2@gmail.com>
2016-10-23 16:18:11 -0400

    Added new config folder and file. I felt this was necessary because of all the requires and includes we have everywhere. This should help aleviate the issue. We can make it more robust in Proj2, maybe including what Brian mentioned about centralized package paths, which would work now that we figured out how to resolve the absolute root path.

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Defines a variable called PROJROOT which contains an absolute
+// path to the root of the project
+//
+// __FILE__ is a magic constant used to retrieve the absolute path to
+// the current running script. It will equal the correct file, even
+// when called through an include().
+//
+// By resolving the path to the root directory relative to the /config
+// directory, we can find the absolute project path no matter what file
+// the config is included from.
+define('PROJROOT', realpath(dirname(__FILE__).'/..'));
+
+?>
