7123446589f13ab07ac04ca90935ae170c242919
Paul McCarthy <armitron2@gmail.com>
2016-10-24 21:01:13 -0400

    Fixes to advisor class.

--- class/Advisor.php
+++ class/Advisor.php
@@ -1,17 +1,14 @@
 <?php
 require('../../class/helper/CommonMethods.php');
-class Appointment {
-
-  
-  public $id;
+class Advisor {
+    public $id;
     public $firstName;
     public $lastName;
     public $userName;
     private $password;
     public $email;
 
-
-    public function __construct($fn, $ln, $un, $pw, $e) {
+    public function __construct($firstName, $lastName, $username, $password, $email) {
         $this->firstName = $fn;
         $this->lastName = $ln;
         $this->userName = $un;
@@ -23,18 +20,22 @@ class Appointment {
       $db = new Common(false);
 
       //verify advisor doesn't already exist in database
-      $sql = "SELECT * FROM 'Advisor' WHERE 'Username' = '".$this->username."'";
+      $sql = "
+      SELECT *
+      FROM 'Advisor'
+      WHERE 'Username' = '".$this->username."'";
       $result = $db->executeQuery($sql, "Add Advisor");
 
-      if (mysql_num_rows($result) == 0){
-	//if no rows returned, no username found, continue with insert
-
-	$sql = "INSERT INTO `Advisor`(`ID`, `Username`, `Password`, `Email`, `Salt`) VALUES ('','".$this->userName."','".$this->password."','".$this->email."','')";
-	$db->executeQuery($sql,"Register Advisor");
-	echo "<script>alert('You have been added.');</script>";
-      }
-      else {
-	echo "Username taken, please choose another.";
+      if (mysql_num_rows($result) == 0) {
+          //if no rows returned, no username found, continue with insert
+          $sql = "
+          INSERT INTO `Advisor`(`ID`, `Username`, `Password`, `Email`, `Salt`)
+          VALUES ('','".$this->userName."','".$this->password."','".$this->email."','')
+          ";
+          $db->executeQuery($sql,"Register Advisor");
+          echo "<script>alert('You have been added.');</script>";
+      } else {
+          echo "Username taken, please choose another.";
       }
     }
 }
